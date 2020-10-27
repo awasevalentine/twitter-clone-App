2936611832fc0238b16a9b4b38b3407f96f2d9a1
awasevalentine <awasevalentine@gmail.com>
2020-10-27 22:32:50 +0100

    initialised a repository for twitter front end

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PageLayoutComponent } from './Pages/Components/page-layout/page-layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+  path: 'layout', components: PageLayoutComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularMaterialModule } from './Pages/MaterialModule/angular-material/angular-material.module';
+import { AccountCreationComponent } from './Pages/Auth/account-creation/account-creation.component';
+import { UserLoginComponent } from './Pages/Auth/user-login/user-login.component';
+import { DashboardComponent } from './Pages/Components/dashboard/dashboard.component';
+import { PageLayoutComponent } from './Pages/Components/page-layout/page-layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccountCreationComponent,
+    UserLoginComponent,
+    DashboardComponent,
+    PageLayoutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AngularMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
