e30f21bfd6d5922fba0dd42f10cc7443a215b139
Bassey <Simon.Bassey@stanbicibtc.com>
2020-10-28 02:26:35 +0100

    created page layout component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PageLayoutComponent } from './Pages/Components/page-layout/page-layout.component';
+import { HomePageComponent } from './Pages/Components/home-page/home-page.component';
 
 const routes: Routes = [
   {
-  path: 'layout', components: PageLayoutComponent
+  path:"layout", component: PageLayoutComponent
+  },
+  {
+    path: "home", component: HomePageComponent
   }
 ];
 
